893ae65f51ca16b3beafddc4b51e6334cc57f99a
Nick Pitrak <pitrak1@gmail.com>
2025-10-18 10:29:56 -0500

    add as parsing for except isntruction

--- Battlescript/Instructions/Exceptions/ExceptInstruction.cs
+++ Battlescript/Instructions/Exceptions/ExceptInstruction.cs
@@ -5,17 +5,28 @@ namespace Battlescript;
 public class ExceptInstruction : Instruction
 {
     public Instruction? Value { get; set; }
+    public VariableInstruction? ExceptionVariable { get; set; }
 
     public ExceptInstruction(List<Token> tokens) : base(tokens)
     {
-        // Want to ignore except keyword at start and colon at end
-        Value = InstructionFactory.Create(tokens.GetRange(1, tokens.Count - 2));
+        var asIndex = InstructionUtilities.GetTokenIndex(tokens, ["as"]);
+        if (asIndex == -1)
+        {
+            // Want to ignore except keyword at start and colon at end
+            Value = InstructionFactory.Create(tokens.GetRange(1, tokens.Count - 2));
+        }
+        else
+        {
+            Value = InstructionFactory.Create(tokens.GetRange(1, asIndex - 1));
+            ExceptionVariable = InstructionFactory.Create(tokens.GetRange(asIndex + 1, tokens.Count - asIndex - 1)) as VariableInstruction;
+        }
     }
     
-    public ExceptInstruction(Instruction value, List<Instruction> instructions) : base([])
+    public ExceptInstruction(Instruction value, List<Instruction> instructions, VariableInstruction? exceptionVariable = null) : base([])
     {
         Value = value;
         Instructions = instructions;
+        ExceptionVariable = exceptionVariable;
     }
     
     public override Variable? Interpret(
--- BattlescriptTests/Instructions/TryInstructionTests.cs
+++ BattlescriptTests/Instructions/TryInstructionTests.cs
@@ -123,133 +123,26 @@ public static class TryInstructionTests
             new FinallyInstruction(new List<Instruction>() { new AssignmentInstruction("=", new VariableInstruction("x"), new NumericInstruction(5))}))};
             Assertions.AssertInputProducesParserOutput(input, expected);
         }
+        
+        [Test]
+        public void ProperlyParsesTryExceptWithAs()
+        {
+            var input = """
+                        try:
+                            x = 1
+                        except 1 as f:
+                            x = 2
+                        """;
+            var expected = new List<Instruction>() {new TryInstruction([
+                new AssignmentInstruction("=", new VariableInstruction("x"), new NumericInstruction(1))
+            ], new List<ExceptInstruction>()
+            {
+                new ExceptInstruction(
+                    new NumericInstruction(1), 
+                    [new AssignmentInstruction("=", new VariableInstruction("x"), new NumericInstruction(2))],
+                    new VariableInstruction("f"))
+            })};
+            Assertions.AssertInputProducesParserOutput(input, expected);
+        }
     }
-    
-    // Will comment these out and fix when stacktrace stuff is tested
-    // [TestFixture]
-    // public class Interpret
-    // {
-    //     [Test]
-    //     public void RunsElseIfTryThrowsException()
-    //     {
-    //         var memory = Runner.Run("""
-    //                                 x = 1
-    //                                 try:
-    //                                     raise 1
-    //                                 else:
-    //                                     x = 'asdf'
-    //                                 """);
-    //         Assertions.AssertVariable(memory, "x", memory.Create(Memory.BsTypes.String, "asdf"));
-    //     }
-    //     
-    //     [Test]
-    //     public void RunsExceptIfTryThrowsExceptionThatMatches()
-    //     {
-    //         var memory = Runner.Run("""
-    //                                 x = 1
-    //                                 try:
-    //                                     raise Exception('asdf')
-    //                                 except Exception:
-    //                                     x = 'qwer'
-    //                                 """);
-    //         Assertions.AssertVariable(memory, "x", memory.Create(Memory.BsTypes.String, "qwer"));
-    //     }
-    //     
-    //     [Test]
-    //     public void DoesNotRunElseIfTryThrowsExceptionThatMatches()
-    //     {
-    //         var memory = Runner.Run("""
-    //                                 x = 1
-    //                                 try:
-    //                                     raise Exception('asdf')
-    //                                 except Exception:
-    //                                     x = 'qwer'
-    //                                 else:
-    //                                     x = 'asdf'
-    //                                 """);
-    //         Assertions.AssertVariable(memory, "x", memory.Create(Memory.BsTypes.String, "qwer"));
-    //     }
-    //     
-    //     [Test]
-    //     public void DoesNotRunExceptIfTryThrowsExceptionThatDoesNotMatch()
-    //     {
-    //         var memory = Runner.Run("""
-    //                                 x = 1
-    //                                 try:
-    //                                     raise Exception('asdf')
-    //                                 except list:
-    //                                     x = 'qwer'
-    //                                 else:
-    //                                     x = 'asdf'
-    //                                 """);
-    //         Assertions.AssertVariable(memory, "x", memory.Create(Memory.BsTypes.String, "asdf"));
-    //     }
-    //     
-    //     [Test]
-    //     public void RunsMatchingExceptIfTryThrowsException()
-    //     {
-    //         var memory = Runner.Run("""
-    //                                 x = 1
-    //                                 try:
-    //                                     raise Exception('asdf')
-    //                                 except list:
-    //                                     x = 'qwer'
-    //                                 except Exception:
-    //                                     x = 'zxcv'
-    //                                 else:
-    //                                     x = 'asdf'
-    //                                 """);
-    //         Assertions.AssertVariable(memory, "x", memory.Create(Memory.BsTypes.String, "zxcv"));
-    //     }
-    //     
-    //     [Test]
-    //     public void RunsFinallyAfterTryBlockIfTryDoesNotThrowException()
-    //     {
-    //         var memory = Runner.Run("""
-    //                                 x = 1
-    //                                 try:
-    //                                     x = 2
-    //                                 finally:
-    //                                     x = 3
-    //                                 """);
-    //         Assertions.AssertVariable(memory, "x", memory.Create(Memory.BsTypes.Int, 3));
-    //     }
-    //     
-    //     [Test]
-    //     public void RunsFinallyAfterExceptBlockIfTryThrowsMatchingException()
-    //     {
-    //         var memory = Runner.Run("""
-    //                                 x = 1
-    //                                 y = 1
-    //                                 try:
-    //                                     raise Exception('asdf')
-    //                                 except Exception:
-    //                                     y = 2
-    //                                 finally:
-    //                                     x = 2
-    //                                 """);
-    //         Assertions.AssertVariable(memory, "x", memory.Create(Memory.BsTypes.Int, 2));
-    //         Assertions.AssertVariable(memory, "y", memory.Create(Memory.BsTypes.Int, 2));
-    //     }
-    //     
-    //     [Test]
-    //     public void RunsFinallyAfterElseBlockIfTryThrowsException()
-    //     {
-    //         var memory = Runner.Run("""
-    //                                 x = 1
-    //                                 y = 1
-    //                                 try:
-    //                                     raise Exception('asdf')
-    //                                 else:
-    //                                     y = 2
-    //                                 finally:
-    //                                     x = 2
-    //                                 """);
-    //         Assertions.AssertVariable(memory, "x", memory.Create(Memory.BsTypes.Int, 2));
-    //         Assertions.AssertVariable(memory, "y", memory.Create(Memory.BsTypes.Int, 2));
-    //     }
-    //     
-    //     // We'll have to write some additional tests here for running finally block if exceptions are raised in
-    //     // except or else blocks
-    // }
 }
